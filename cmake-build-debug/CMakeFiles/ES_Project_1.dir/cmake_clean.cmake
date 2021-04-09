66aa4721415fe5ae71386e88719b23c38f270b8a
yusufsherif <yusufsherif@outlook.com>
2021-04-09 21:05:46 +0200

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/ES_Project_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ES_Project_1.dir/main.c.o"
+  "ES_Project_1"
+  "ES_Project_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ES_Project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
